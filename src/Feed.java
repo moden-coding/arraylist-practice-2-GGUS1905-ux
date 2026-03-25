a5e8c3a4d50381f40f67f85337cafe996c6ee85e
Guy <29ggus@gannacademy.org>
2026-03-25 13:44:44 -0400

    done

--- src/Feed.java
+++ src/Feed.java
@@ -4,6 +4,18 @@ public class Feed {
     public static void main(String[] args) {
           // Step 1: Create your ArrayList to hold post descriptions
         ArrayList<String> feed = new ArrayList<String>();
+        System.out.println("--- Top of Feed ---");
+        feed.add("Guy posted a story");
+        feed.add("Shaya requested to follow you");
+        feed.add("Eliora added you back");
+        feed.add("Gabe snapped you back");
+        feed.add("Elizabeth posted a video");
+        feed.add("Ella wants to be your friend");
+        feed.add("DR.Moden started following you");
+        for(int i = 0;i<4;i++){
+            System.out.println(feed.get(i));
+        }
+
 
         // Step 2: Add 7 post descriptions using .add()
         // Example: feed.add("Maya posted a photo");
